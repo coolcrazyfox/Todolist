0fe9dcc76164943aba51b204f84b1b74c7b0c0aa
Andrei Malets <coolviktori@yandex.by>
2021-11-14 17:22:30 +0300

    add Tolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import Todolist from "./Todolist";
+import {Tolist} from "./Tolist";
 
 export  type TaskType ={
     id: number
     title: string
     isDone:boolean
 }
+
 function App() {
+    const toList="Books that I know"
+
+
     const task_1: Array<TaskType> =[
         {id: 1, title: "HTML", isDone: true},
         {id: 2, title: "CSS", isDone: true},
@@ -17,13 +22,17 @@ function App() {
     const task_2: Array<TaskType> =[
         {id: 4, title: "VODKA", isDone: true},
         {id: 5, title: "Beer", isDone: true},
-        {id: 6, title: "Сhampagne", isDone: true}
-
+        {id: 6, title: "Champagne", isDone: true}
+    ]
+    const task_3: Array<TaskType> =[
+        {id: 7, title: "Alphabet", isDone: false},
+        {id: 8, title: "Is there life on Mars...?", isDone: true}
     ]
     return (
         <div className="App">
             <Todolist title={"What to learn"} task={task_1}/>
-            <Todolist title={"What to buy"} task={task_2}/>            
+            <Todolist title={"What to buy"} task={task_2}/>
+            <Tolist title={toList} task={task_3}/>
         </div>
     );
 }

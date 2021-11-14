0e8a106aaa75fc73d45b9a74f56acbc79cb66af6
Andrei Malets <coolviktori@yandex.by>
2021-11-14 15:53:01 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
     const task_2: Array<TaskType> =[
         {id: 4, title: "VODKA", isDone: true},
         {id: 5, title: "Beer", isDone: true},
-        {id: 6, title: "Shampan", isDone: false}
+        {id: 6, title: "Сhampagne", isDone: false}
 
     ]
     return (

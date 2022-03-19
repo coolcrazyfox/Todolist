747667931a81be23b1ac70b0a27f981211bd587b
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-03-19 15:00:34 +0300

    Updates

---
+++ precache-manifest.27eb7ee65ac3eb8eb988faaecca73f6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ad67b412eef14c2d52474868b479be",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "16e951bdbbce505d0901",
+    "url": "/Todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "2e792d8e96f57a78a28d",
+    "url": "/Todolist/static/js/2.4ae4d400.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/Todolist/static/js/2.4ae4d400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e951bdbbce505d0901",
+    "url": "/Todolist/static/js/main.21680dd0.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
